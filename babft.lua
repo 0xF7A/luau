2c576d6a1123f0ba095265f3bbe6a05deacb448a
0xF7A <fstxzero@gmail.com>
2026-02-25 17:47:19 +0700

    Update SCRIPT_URL to point to the correct file

--- babft.lua
+++ babft.lua
@@ -9,7 +9,7 @@ local lp = Players.LocalPlayer
 local TWEEN_SPEED = 150
 local WAIT_TIME = 1
 local HOP_INTERVAL = 3600
-local SCRIPT_URL = "https://raw.githubusercontent.com/0xF7A/luau/refs/heads/main/babft.luau"
+local SCRIPT_URL = "https://raw.githubusercontent.com/0xF7A/lua/refs/heads/main/babft.lua"
 
 local positions = {
     CFrame.new(-58.5114212, 95.066906, 307.004639, -0.999982238, -0.000746380421, 0.005914988, -7.38730321e-09, 0.992132723, 0.125190616, -0.00596189313, 0.125188395, -0.99211508),
